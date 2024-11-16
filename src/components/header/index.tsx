fbd91b8ed72f2c1fb699be9f13af061f89bdb4bd
Leonardo Araujo Sarmento <leosarmento@ucl.br>
2024-11-16 16:58:23 -0300

    refactor(typograghy): added components for each of the typography tags

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,5 @@
+import { H1 } from '@components/common/typography/h1';
+import { H4 } from '@components/common/typography/h4';
 import { Skeleton } from '@components/ui/skeleton';
 import { cn } from '@lib/utils';
 import { HTMLAttributes } from 'react';
@@ -22,15 +24,11 @@ export default function Header({
 }: HeaderProps) {
   return (
     <div className={cn('my-6 flex flex-col gap-3', props.className)} {...props}>
-      <h1
-        className={cn('scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl gap-3 flex', classNameTitle)}
-      >
+      <H1 className={cn('flex gap-3 text-3xl lg:text-4xl', classNameTitle)}>
         {title}
         {isLoading ? <Skeleton className="h-10 w-[350px] rounded-sm" /> : user ? user : null}
-      </h1>
-      <h4 className={cn('scroll-m-20 text-xl font-semibold tracking-tight', classNameDescription)}>
-        {description}
-      </h4>
+      </H1>
+      {description ? <H4 className={cn('', classNameDescription)}>{description}</H4> : null}
     </div>
   );
 }
